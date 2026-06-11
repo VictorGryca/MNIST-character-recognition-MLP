d6b22088afebdee3458c74c77d8c0214b91f43c9
Victor Grycajuk <victorgryca@gmail.com>
2026-06-10 23:41:08 -0300

    feat: adiciona forward pass principal

--- mlp/network.py
+++ mlp/network.py
@@ -19,3 +19,19 @@ class MLP:
 
         self.W3 = np.random.randn(10, 128) * 0.01
         self.b3 = np.zeros(10)
+
+
+    def forward(self, x):
+        # camada 1
+        self.z1 = self.W1 @ x + self.b1
+        self.a1 = self.act.relu(self.z1)
+
+        # camada 2
+        self.z2 = self.W2 @ self.a1 + self.b2
+        self.a2 = self.act.relu(self.z2)
+
+        # camada de saída
+        self.z3 = self.W3 @ self.a2 + self.b3
+        self.a3 = self.act.softmax(self.z3)
+
+        return self.a3
